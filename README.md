63124ed39b315fb8ff6c0ca7a717ec35afc179ad
Heyjuke58 <H_Hinrichs@gmx.de>
2021-12-19 15:17:12 +0100

    try elasticsearch with reduced corpus

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # tinse_probing_datasets
 Script(s) to generate datasets for probing tasks of BERT (project TINSE)
+
+## Useful commands
+
+#### Run script on server:
+```tmux```
+run script
+
+To quit tmux session:
+Ctrl+b then d
+
+To reattach to tmux session:
+```tmux attach```
+
+#### Get large files from google drive:
+
+```gdown https://drive.google.com/uc?id=<file_id>```
--- src/msmarco_dataset_creation.py
+++ src/msmarco_dataset_creation.py
@@ -1,3 +1,13 @@
+import en_core_web_sm
+from collections import Counter
+from spacy import displacy
+import spacy
+import logging
+from nltk import data, download
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
+from rank_bm25 import BM25Okapi
 from typing import List, Dict, Tuple
 import ftfy
 import random
@@ -10,30 +20,24 @@ import json
 import pandas as pd
 import numpy as np
 from pandas.core.frame import DataFrame
+from easy_elasticsearch import ElasticSearchBM25
 
-from rank_bm25 import BM25Okapi
-from nltk.stem import PorterStemmer
-from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
-from nltk import download
+# set visible devices to -1 since no gpu is needed
+import os
 
-import spacy
-from spacy import displacy
-from collections import Counter
-import en_core_web_sm
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
 ner_nlp = en_core_web_sm.load()
-
 
 download("stopwords")
 download("punkt")
 
 STOPWORDS = set(stopwords.words("english"))
-POTERT_STEMMER = PorterStemmer()
+PORTER_STEMMER = PorterStemmer()
 
-PATH_CORPUS = './assets/msmarco/passage_re_ranking/collection.tsv'
-# PATH_CORPUS = ("./assets/msmarco/passage_re_ranking/collection_sample.tsv") 
-# PATH_CORPUS = ("./assets/msmarco/passage_re_ranking/collection_sample_orig.tsv") 
-PATH_QUERIES = ("./assets/msmarco/passage_re_ranking/queries.dev.tsv")
+# PATH_CORPUS = "./assets/msmarco/passage_re_ranking/collection.tsv"
+PATH_CORPUS = "./assets/msmarco/passage_re_ranking/collection_sample_orig.tsv"
+PATH_QUERIES = "./assets/msmarco/passage_re_ranking/queries.dev.tsv"
 PATH_TOP1000 = "./assets/msmarco/passage_re_ranking/top1000.dev"
 
 
@@ -46,7 +50,7 @@ parser.add_argument(
     "--size",
     type=int,
     dest="size",
-    default=10000,
+    default=10,
     help="Size of the generated dataset.",
 )
 parser.add_argument(
@@ -54,7 +58,7 @@ parser.add_argument(
     "--max_samples_per_query",
     type=int,
     dest="max_samples_per_query",
-    default=5,
+    default=100,
     help="Determines the maximumn number of passage samples with the same query in the generated dataset.",
 )
 parser.add_argument(
@@ -87,7 +91,7 @@ parser.add_argument(
     type=str,
     dest="output_filename",
     default=OUT_MS_MARCO,
-    help="Output filename of the generated dataset"
+    help="Output filename of the generated dataset",
 )
 args = parser.parse_args()
 
@@ -101,24 +105,35 @@ def timing(description: str) -> None:
 
 
 def tokenize_corpus(path: Path) -> pd.DataFrame:
-    corpus_df = pd.read_csv(path, sep="\t", header=None, names=["pid", "passage"], encoding='utf8', dtype={"pid": "int64", "passage": "string"})
-    corpus_df["passage"] = corpus_df["passage"].apply(ftfy.fix_text) # fix unicode errors
-    corpus_df["preprocessed_passage"] = corpus_df["passage"].apply(word_tokenize)
-    corpus_df["preprocessed_passage"] = corpus_df["preprocessed_passage"].apply(
-        lambda x: [word for word in x if word.isalnum()]
+    corpus_df = pd.read_csv(
+        path,
+        sep="\t",
+        header=None,
+        names=["pid", "passage"],
+        encoding="utf8",
+        dtype={"pid": "int64", "passage": "string"},
     )
-    corpus_df["preprocessed_passage"] = corpus_df["preprocessed_passage"].apply(
-        lambda x: [word for word in x if not word in STOPWORDS]
-    )
-    corpus_df["preprocessed_passage"] = corpus_df["preprocessed_passage"].apply(
-        lambda x: [POTERT_STEMMER.stem(word) for word in x]
-    )
+    # fix unicode errors
+    corpus_df["passage"] = corpus_df["passage"].apply(ftfy.fix_text)
+    # corpus_df["preprocessed_passage"] = corpus_df["passage"].apply(word_tokenize)
+    # corpus_df["preprocessed_passage"] = corpus_df["preprocessed_passage"].apply(
+    #     lambda x: [word for word in x if word.isalnum()]
+    # )
+    # corpus_df["preprocessed_passage"] = corpus_df["preprocessed_passage"].apply(
+    #     lambda x: [word for word in x if not word in STOPWORDS]
+    # )
+    # corpus_df["preprocessed_passage"] = corpus_df["preprocessed_passage"].apply(
+    #     lambda x: [PORTER_STEMMER.stem(word) for word in x]
+    # )
+    logging.info("Corpus preprocessed.")
 
     return corpus_df
 
 
 def tokenize_queries(path: Path) -> pd.DataFrame:
-    queries_df = pd.read_csv(path, sep="\t", header=None, names=["qid", "query"], encoding='utf8')
+    queries_df = pd.read_csv(
+        path, sep="\t", header=None, names=["qid", "query"], encoding="utf8"
+    )
     queries_df["preprocessed_query"] = queries_df["query"].apply(word_tokenize)
     queries_df["preprocessed_query"] = queries_df["preprocessed_query"].apply(
         lambda x: [word for word in x if word.isalnum()]
@@ -127,38 +142,83 @@ def tokenize_queries(path: Path) -> pd.DataFrame:
         lambda x: [word for word in x if not word in STOPWORDS]
     )
     queries_df["preprocessed_query"] = queries_df["preprocessed_query"].apply(
-        lambda x: [POTERT_STEMMER.stem(word) for word in x]
+        lambda x: [PORTER_STEMMER.stem(word) for word in x]
     )
+    logging.info("Queries preprocessed.")
 
     return queries_df
 
+
 def get_top_1000_passages(path: Path) -> Dict[int, List[int]]:
-    # not all queries have 1000 associated passages (sometimes only 1)!
-    # df = pd.read_csv(path, sep="\t", header=None, names=["qid", "pid"], usecols=[0, 1])
-
-    # sanity check
-    # unique_qids = df['qid'].unique()
-    # too_less = []
-    # for unique_qid in np.nditer(unique_qids):
-    #     filtered_df = df.loc[df["qid"] == unique_qid]
-    #     if filtered_df.shape[0] != 1000:
-    #         too_less.append(filtered_df.shape[0])
-    # print(min(too_less))
-
-    q_p_top1000_dict: Dict[int, List[int]] = defaultdict(list) # q_id -> [p_id1, p_id2, .. , p_id1000]
-    with open(path, 'r') as f:
+    # q_id -> [p_id1, p_id2, .. , p_id1000]
+    q_p_top1000_dict: Dict[int, List[int]] = defaultdict(list)
+    with open(path, "r") as f:
         for line in f:
             q_id, p_id = tuple(line.split(sep="\t")[:2])
             q_p_top1000_dict[int(q_id)].append(int(p_id))
-    
+    logging.info("Top 1000 passages per query parsed.")
+
     return q_p_top1000_dict
+
 
 def set_new_index(df: DataFrame) -> DataFrame:
     df["idx"] = pd.Int64Index(range(df.shape[0]))
     return df.set_index("idx")
 
 
-def build_bm25_dataset_from_dataframe(df: DataFrame, source: str) -> List[Dict]:
+def sample_queries_and_passages(
+    corpus_df: pd.DataFrame,
+    query_df: pd.DataFrame,
+    q_p_top1000: Dict[int, List[int]],
+    size: int,
+    max_samples_per_query: int,
+) -> pd.DataFrame:
+    rand_queries = pd.DataFrame()
+    rand_passages = pd.DataFrame()
+
+    sampled_queries = random.sample(list(q_p_top1000), len(q_p_top1000.keys()))
+
+    for qid in sampled_queries:
+        possible_passages = q_p_top1000[qid]
+        sample_size = (
+            max_samples_per_query
+            if len(possible_passages) >= max_samples_per_query
+            else len(possible_passages)
+        )
+        sampled_passages = random.sample(possible_passages, sample_size)
+        preprocessed_sampled_passages = corpus_df.loc[
+            corpus_df["pid"].isin(sampled_passages)
+        ]
+        rand_passages = rand_passages.append(preprocessed_sampled_passages)
+        preprocessed_sampled_queries = query_df.loc[query_df["qid"] == qid]
+        rand_queries = rand_queries.append(
+            [preprocessed_sampled_queries] * len(preprocessed_sampled_passages),
+            ignore_index=True,
+        )
+
+        if len(rand_passages) >= size:
+            rand_passages = rand_passages[:size]
+            rand_queries = rand_queries[:size]
+            break
+
+    assert (
+        len(rand_passages) == size
+    ), f"Dataset cannot have specified size of {size}. Please lower the value to at least {len(rand_passages)} or increase the maximumn number of samples per query."
+
+    # reset index to merge with queries
+    rand_passages = set_new_index(rand_passages)
+    rand_queries = set_new_index(rand_queries)
+
+    # concat passages and queries dataframes
+    passage_query_df = pd.concat([rand_passages, rand_queries], sort=False, axis=1)
+    logging.info(
+        f"Dataset sample of size {size} with max samples per query of {max_samples_per_query} generated."
+    )
+
+    return passage_query_df
+
+
+def encode_bm25_dataset_to_json(df: DataFrame, source: str) -> List[Dict]:
     dataset: List[Dict] = []
     for idx, row in df.iterrows():
         dataset.append(
@@ -173,11 +233,14 @@ def build_bm25_dataset_from_dataframe(df: DataFrame, source: str) -> List[Dict]:
                 "target": row["bm25"],
             }
         )
+    logging.info("BM25 dataset encoded to json.")
 
     return dataset
 
 
-def build_ner_dataset_from_dataframe(df: DataFrame, df_targets: DataFrame, source: str) -> List[Dict]:
+def encode_ner_dataset_to_json(
+    df: DataFrame, targets: Dict[int, List[Tuple[List, str]]], source: str
+) -> List[Dict]:
     dataset: List[Dict] = []
     for idx, row in df.iterrows():
         dataset.append(
@@ -190,16 +253,17 @@ def build_ner_dataset_from_dataframe(df: DataFrame, df_targets: DataFrame, sourc
                 "text": row["query"] + " [SEP] " + row["passage"],
                 "input": {"passage": row["passage"], "query": row["query"]},
                 "targets": [
-                    {"span1": [row2["start"], row2["end"]], "label": row2["label"]} 
-                    for idx2, row2 in df_targets[df_targets["pid"] == row["pid"]].iterrows()
+                    {"span1": start_end, "label": label}
+                    for start_end, label in targets[row["pid"]]
                 ],
             }
         )
+    logging.info("NER dataset encoded to json.")
 
-    return dataset 
+    return dataset
 
 
-def build_sem_sim_dataset_from_dataframe(df: DataFrame, source: str) -> List[Dict]:
+def encode_sem_sim_dataset_to_json(df: DataFrame, source: str) -> List[Dict]:
     dataset: List[Dict] = []
     for idx, row in df.iterrows():
         dataset.append(
@@ -214,104 +278,94 @@ def build_sem_sim_dataset_from_dataframe(df: DataFrame, source: str) -> List[Dic
                 "target": row["cos_sim"],
             }
         )
+    logging.info("Semantic similarity dataset encoded to json.")
 
-    return dataset 
+    return dataset
 
 
 def write_dataset_to_file(path: Path, dataset) -> None:
-    with open(path, "w", encoding='utf8') as outfile:
+    with open(path, "w", encoding="utf8") as outfile:
         json.dump(dataset, outfile, indent=4, ensure_ascii=False)
 
 
-def bm25_dataset_creation(size: int, max_samples_per_query: int, corpus_df: DataFrame, queries_df: DataFrame, source: str, output_filename: str) -> None:
-    bm25 = BM25Okapi(list(corpus_df["preprocessed_passage"]))
-    q_p_top1000 = get_top_1000_passages(PATH_TOP1000) # get dict of the top 1000 passages per query
-
-    rand_queries = pd.DataFrame()
-    rand_passages = pd.DataFrame()
-    
-    sampled_queries = random.sample(list(q_p_top1000), len(q_p_top1000.keys()))
-    
-    for qid in sampled_queries:
-        possible_passages = q_p_top1000[qid]
-        sample_size = max_samples_per_query if len(possible_passages) >= max_samples_per_query else len(possible_passages)
-        sampled_passages = random.sample(possible_passages, sample_size)
-        preprocessed_sampled_passages = corpus_df.loc[corpus_df['pid'].isin(sampled_passages)]
-        rand_passages = rand_passages.append(preprocessed_sampled_passages)
-        preprocessed_sampled_queries = queries_df.loc[queries_df['qid'] == qid]
-        rand_queries = rand_queries.append([preprocessed_sampled_queries] * len(preprocessed_sampled_passages), ignore_index=True)
-        
-        if len(rand_passages) >= size:
-            rand_passages = rand_passages[:size]
-            rand_queries = rand_queries[:size]
-            break
-    
-    assert len(rand_passages) == size, f"Dataset cannot have specified size of {size}. Please lower the value to at least {len(rand_passages)} or increase the maximumn number of samples per query."
-
-
-    # reset index to merge with queries
-    rand_passages = set_new_index(rand_passages)
-    rand_queries = set_new_index(rand_queries)
-
-    # concat passages and queries dataframes
-    passage_query_df = pd.concat([rand_passages, rand_queries], sort=False, axis=1)
-
-    # calculate bm25 scores
-    passage_query_df["bm25"] = passage_query_df.apply(
-        lambda x: bm25.get_scores(x["preprocessed_query"])[x["pid"]], axis=1
+def bm25_dataset_creation(
+    dataset_df: pd.DataFrame,
+    corpus_df: pd.DataFrame,
+    source: str,
+    output_filename: str,
+) -> None:
+    pool = corpus_df["passage"].to_dict()
+    bm25 = ElasticSearchBM25(
+        pool, host="192.168.1.80", port_http="9206", port_tcp="9206"
     )
 
-    dataset_dict = build_bm25_dataset_from_dataframe(passage_query_df, source)
+    # free memory
+    del pool
+    del corpus_df
+
+    # bm25 = BM25Okapi(list(corpus_df["preprocessed_passage"]))
+
+    # calculate bm25 scores
+    dataset_df["bm25"] = dataset_df.apply(
+        lambda x: bm25.score(x["preprocessed_query"], document_ids=[x["pid"]])[
+            x["pid"]
+        ],
+        axis=1,
+    )
+
+    dataset_dict = encode_bm25_dataset_to_json(dataset_df, source)
 
     write_dataset_to_file(Path("./datasets/") / output_filename, dataset_dict)
+    logging.info(f"BM25 dataset saved to ./datasets/{output_filename}")
 
 
-def ner_dataset_creation(size: int, max_samples_per_query: int, corpus_df: DataFrame, source: str, output_filename: str) -> None:
-    df_passages: Dict[int, str] = {}
-    df_targets: List[Tuple[int, List, str]] = [] # list of (pid, [start, end], label)
+def ner_dataset_creation(
+    dataset_df: pd.DataFrame, source: str, output_filename: str,
+) -> None:
+    # key: pid, value: List[([start, end], label)]
+    df_targets: Dict[int, List[Tuple[List, str]]] = {}
 
-    # sample passages for the dataset
-    sampled_pids = corpus_df["pid"].sample(size)
+    for idx, row in dataset_df.iterrows():
+        doc = ner_nlp(row["passage"])
+        new_targets = [([X.start, X.end], X.label_) for X in doc.ents]
+        df_targets[row["pid"]] = new_targets
 
-    # extract named entities
-    for pid in sampled_pids:
-        passage = corpus_df.loc[pid].passage
-        df_passages[pid] = passage
-        doc = ner_nlp(passage)
-        for X in doc.ents:
-            start = passage.find(X.test)
-            df_targets.append()
-        df_targets.extend([(pid, [passage.find(X.text), ], X.label) for X in doc.ents])
-        print([(X.text, X.label_) for X in doc.ents])
+    dataset_dict = encode_ner_dataset_to_json(dataset_df, df_targets, source)
+    write_dataset_to_file(Path("./datasets") / output_filename, dataset_dict)
+    logging.info(f"NER dataset saved to ./datasets/{output_filename}")
 
-    dataset_dict = build_ner_dataset_from_dataframe(df, df_targets)
+
+def sem_sim_dataset_creation(
+    dataset_df: pd.DataFrame, source: str, output_filename: str,
+) -> None:
+
+    dataset_dict = encode_sem_sim_dataset_to_json(df, df_targets)
     write_dataset_to_file(Path("./dataset"), output_filename, dataset_dict)
-
-
-def sem_sim_dataset_creation(size: int, max_samples_per_query: int, corpus_df: DataFrame, queries_df: DataFrame, source: str, output_filename: str) -> None:
-
-
-    dataset_dict = build_sem_sim_dataset_from_dataframe(df, df_targets)
-    write_dataset_to_file(Path("./dataset"), output_filename, dataset_dict)
-
+    logging.info(f"Semantic similarity dataset saved to ./datasets/{output_filename}")
 
 
 if __name__ == "__main__":
-    corpus_df = tokenize_corpus(Path(args.path_corpus))  # preprocess corpus (passages) for bm25
-    queries_df = tokenize_queries(Path(args.path_queries))  # preprocess quueries for bm25
-    
-    bm25_dataset_creation(
-        args.size,
-        args.max_samples_per_query,
-        corpus_df,
-        queries_df,
-        args.source, 
-        args.output_filename
+    logging.basicConfig(filename="msmarco.log", filemode="w", level=logging.INFO)
+
+    # preprocess corpus (passages) for bm25
+    corpus_df = tokenize_corpus(Path(args.path_corpus))
+    # preprocess quueries for bm25
+    query_df = tokenize_queries(Path(args.path_queries))
+    # dict query to relevant passages
+    q_p_top1000_dict = get_top_1000_passages(PATH_TOP1000)
+
+    dataset_df = sample_queries_and_passages(
+        corpus_df, query_df, q_p_top1000_dict, args.size, args.max_samples_per_query
     )
-    # ner_dataset_creation(
-    #     args.size,
-    #     args.max_samples_per_query,
-    #     corpus_df,
-    #     args.source,
-    #     args.output_filename
-    # )
+
+    # free memory
+    del query_df
+    del q_p_top1000_dict
+
+    bm25_dataset_creation(dataset_df, corpus_df, args.source, args.output_filename)
+
+    # free memory
+    del corpus_df
+
+    # ner_dataset_creation(dataset_df, args.source, args.output_filename)
+    # sem_sim_dataset_creation(dataset_df, args.source, args.output_filename)
